69eda49284eb312a6402469fdf89d6df6a582e05
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-12-18 21:08:10 +1000

    add remaining components

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
+import Experience from '../components/Experience'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,16 +20,30 @@ export default function Home() {
 
       <Header />
 
-      {/* Hero */} 
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
 
-      {/* About */} 
-      {/* Experience */}
-      {/* Skills */} 
-      {/* Projects */} 
-      {/* Contact Me */} 
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
+
+
+      <section id="experience" className='snap-center'>
+        <Experience />
+      </section>
+
+      <section id="skills" className='snap-start'>
+        <Skills />
+      </section>
+
+      <section id="projects" className='snap-start'>
+        <Projects />
+      </section>
+
+      <section id="contactme" className='snap-start'>
+        <ContactMe />
+      </section>
 
     </div>
 
